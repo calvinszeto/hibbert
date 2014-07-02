7fb41e77f97a3a315f626d53216b0eb70484e5ab
Calvin Szeto <szeto.calvin@gmail.com>
2014-07-02 10:23:42 -0500

    Write out skeleton for sources controller spec.

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,44 +1,16 @@
 class SourcesController < ApplicationController
-  before_action :set_source, only: [:show, :edit, :update, :destroy]
+  before_action :set_source, only: [:show, :update]
 
-  # GET /sources
-  # GET /sources.json
+  # GET /sources?format=json{&location&no_filter}
   def index
     @sources = Source.all
   end
 
-  # GET /sources/1
-  # GET /sources/1.json
+  # GET /sources/1?format=json
   def show
   end
 
-  # GET /sources/new
-  def new
-    @source = Source.new
-  end
-
-  # GET /sources/1/edit
-  def edit
-  end
-
-  # POST /sources
-  # POST /sources.json
-  def create
-    @source = Source.new(source_params)
-
-    respond_to do |format|
-      if @source.save
-        format.html { redirect_to @source, notice: 'Source was successfully created.' }
-        format.json { render :show, status: :created, location: @source }
-      else
-        format.html { render :new }
-        format.json { render json: @source.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /sources/1
-  # PATCH/PUT /sources/1.json
+  # PATCH /sources/1
   def update
     respond_to do |format|
       if @source.update(source_params)
@@ -51,24 +23,8 @@ class SourcesController < ApplicationController
     end
   end
 
-  # DELETE /sources/1
-  # DELETE /sources/1.json
-  def destroy
-    @source.destroy
-    respond_to do |format|
-      format.html { redirect_to sources_url, notice: 'Source was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_source
       @source = Source.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def source_params
-      params.require(:source).permit(:name, :category, :website)
     end
 end
