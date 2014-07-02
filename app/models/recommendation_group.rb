c55ea74cb2382d3f95a0cbe51fa91876781a8779
Calvin Szeto <szeto.calvin@gmail.com>
2014-07-02 11:48:17 -0500

    Update annotation.

--- app/models/recommendation_group.rb
+++ app/models/recommendation_group.rb
@@ -2,12 +2,14 @@
 #
 # Table name: recommendation_groups
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  website    :string(255)
-#  source_id  :integer
-#  created_at :datetime
-#  updated_at :datetime
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  website     :string(255)
+#  source_id   :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#  date        :date
+#  description :text
 #
 
 class RecommendationGroup < ActiveRecord::Base
