04250d76296f93c4cb18fa15a476df195dc8dadd
Calvin Szeto <szeto.calvin@gmail.com>
2014-07-03 15:55:00 -0500

    Add more restaurant specs.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,14 +2,16 @@ class RestaurantsController < ApplicationController
   # GET /restaurants?format=json{&only&except&location&tried&no_filter}
   def index
     @restaurants = Restaurant.all
-		unless params[:tried]
-			@restaurants = @restaurants.not_tried_by(current_user)
-		end
-		unless params[:no_filter]
-			@restaurants = @restaurants.showable(current_user)
-			
-			# Until I figure out how to do this via queries, keep this last
-			@restaurants = @restaurants.to_a.keep_if{|r| r.source_showable(current_user)}
+		unless current_user.nil?
+			unless params[:tried]
+				@restaurants = @restaurants.not_tried_by(current_user)
+			end
+			unless params[:no_filter]
+				@restaurants = @restaurants.showable(current_user)
+				
+				# Until I figure out how to do this via queries, keep this last
+				@restaurants = @restaurants.to_a.keep_if{|r| r.source_showable(current_user)}
+			end
 		end
   end
 
--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -13,9 +13,6 @@ RSpec.describe RestaurantsController, :type => :controller do
 				sign_in @user
 			end
 
-			it "should return restaurants with user preference attributes" do
-			end
-
 			it "should filter by source preferences" do
 				bad_restaurant = FactoryGirl.create(:restaurant)
 				recommendation = FactoryGirl.create(:recommendation,
@@ -50,100 +47,77 @@ RSpec.describe RestaurantsController, :type => :controller do
 
 			context "with :no_filter=true" do
 				it "should return restaurants without filtering on user preferences" do
+				bad_restaurant_one = FactoryGirl.create(:restaurant)
+				recommendation = FactoryGirl.create(:recommendation,
+																						:restaurant => bad_restaurant_one)	
+				bad_restaurant_two = FactoryGirl.create(:restaurant)
+				@user.add_no_show_source(recommendation.source)
+				@user.add_no_show_restaurant(bad_restaurant_two)
+				get :index, no_filter: true, format: :json	
+				expect(assigns(:restaurants).to_a).to eq(Restaurant.all.to_a)
 				end
 			end
-
-			context "with :only param" do
-				it "should add the categories to user preferences" do
-				end
-			end
-
-			context "with :except param" do
-				it "should add the categories to user preferences" do
-				end
-			end
-
-			context "with :location param" do
-				it "should add the location to user preferences" do
-				end
-			end
-
-			it "should give preference to recent categories"
-
-			it "should give preference to recent locations"
 		end
 
 		context "without user signed in" do
-			it "should return restaurants without user preference attributes" do
-			end
-
 			it "should return all restaurants by default" do
+				get :index, format: :json	
+				expect(assigns(:restaurants).to_a).to eq(Restaurant.all.to_a)
 			end
 
 			context "with :no_filter=true" do
 				it "should return all restaurants by default" do
+					get :index, no_filter: true, format: :json	
+					expect(assigns(:restaurants).to_a).to eq(Restaurant.all.to_a)
 				end
 			end
 		end
 
 		context "with :only param" do
-			it "should return all categories if param is empty" do
-			end
+			it "should return all categories if param is empty"
 
-			it "should return only restaurants whose category matches the param" do
-			end
+			it "should return only restaurants whose category matches the param"
 		end
 
 		context "with :except param" do
-			it "should return all categories if param is empty" do
-			end
+			it "should return all categories if param is empty"
 
-			it "should filter out restaurants whose category matches the param" do
-			end
+			it "should filter out restaurants whose category matches the param"
 		end
 
 		context "with :location param" do
-			it "should return restaurants with a distance attribute" do
-			end
+			it "should return restaurants with a distance attribute"
 
-			it "should return restaurants ordered by distance from the param" do
-			end
+			it "should return restaurants ordered by distance from the param"
 
-			it "should return an error if param can't be geocoded" do
-			end	
+			it "should return an error if param can't be geocoded"
 
-			it "should return all restaurants if param can't be geocoded" do
-			end
+			it "should return all restaurants if param can't be geocoded"
 		end
   end
 
   describe "GET show" do
-		it "should return the specified restaurant object" do
+		it "should return the specified restaurant object"
       #restaurant = Restaurant.create! valid_attributes
       #get :show, {:id => restaurant.to_param}, valid_session
       #expect(assigns(:restaurant)).to eq(restaurant)
-		end
 
 		context "with user signed in" do
-			it "should return a restaurant with user preference attributes" do
-			end
+			it "should return a restaurant with user preference attributes"
 		end
 
 		context "without user signed in" do
-			it "should return a restaurant without user preference attributes" do
-			end
+			it "should return a restaurant without user preference attributes"
 		end
   end
 
 	describe "PATCH update" do
 		context "with :tried param" do
-			it "should update user tried preferences" do
-			end
+			it "should update user tried preferences"
 		end
 
 		context "with :do_not_show param" do
-			it "should update user do-not-show preferences" do
-			end
+			it "should update user do-not-show preferences"
 		end
 	end
 
