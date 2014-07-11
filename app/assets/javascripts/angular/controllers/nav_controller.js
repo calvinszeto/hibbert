dc913ad07f6e0eb24b83476a6769bcf723da791d
Calvin Szeto <szeto.calvin@gmail.com>
2014-07-10 22:54:45 -0500

    Write some angular constants and start Restaurant service.

---
+++ app/assets/javascripts/angular/controllers/nav_controller.js
@@ -0,0 +1,3 @@
+app.controller('NavCtrl', [ 'HIBBERT_URL', '$scope', function(HIBBERT_URL, $scope) {
+    console.log(HIBBERT_URL);
+}]);
---
+++ app/assets/javascripts/angular/services/restaurant.js
@@ -0,0 +1,7 @@
+app.factory('Restaurant', ['HIBBERT_URL', '$resource', function (HIBBERT_URL, $resource){
+    var Restaurant = $resource(HIBBERT_URL + 'restaurants/:restaurantId.json', {restaurantId: '@id'}, {
+
+    });
+
+    return Restaurant;
+}]);
