559055e5c214a7ec56127c610c049d1a46f047d3
Calvin Szeto <szeto.calvin@gmail.com>
2014-07-16 10:24:36 -0500

    Add routing and convert restaurant show to use routes.

--- app/assets/javascripts/angular/controllers/restaurant_controller.js
+++ app/assets/javascripts/angular/controllers/restaurant_controller.js
@@ -1,4 +1,4 @@
 app.controller('RestaurantController', ['$scope', 'Restaurant', 'restaurant',
     function($scope, Restaurant, restaurant) {
-    $scope.restaurant = Restaurant.get({restaurantId: restaurant.id});
+    $scope.restaurant = restaurant;
 }]);
