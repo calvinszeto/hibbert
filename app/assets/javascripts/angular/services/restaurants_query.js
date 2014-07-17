51e70e60eaecf5c7c8ab076fcddfb01e05f850dc
Calvin Szeto <szeto.calvin@gmail.com>
2014-07-17 09:34:06 -0500

    Fix restaurant query to persist query filters.

--- app/assets/javascripts/angular/services/restaurants_query.js
+++ app/assets/javascripts/angular/services/restaurants_query.js
@@ -1,9 +1,17 @@
 //This service is for sharing the queried restaurants between the filters modal and the restaurants browser
 app.factory('RestaurantsQuery', ['Restaurant', function (Restaurant) {
     var RestaurantsQuery = {
+        query: {
+            only: [],
+            except: [],
+            location: "",
+            distance: 5,
+            tried: false,
+            no_filter: false
+        },
         restaurants: Restaurant.query(),
-        search: function (filters) {
-            RestaurantsQuery.restaurants = Restaurant.query(filters,
+        search: function () {
+            RestaurantsQuery.restaurants = Restaurant.query(RestaurantsQuery.query,
                 function (data, status, headers, config) {
                     if (data && data[0].hasOwnProperty("distance")) {
                         RestaurantsQuery.hasDistance = true;
