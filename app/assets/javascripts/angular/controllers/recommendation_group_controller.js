4c34766739a2b137bff69d2a3ca0d683a73a7696
Calvin Szeto <szeto.calvin@gmail.com>
2014-07-18 11:06:20 -0500

    Add styling to recommendation groups.

--- app/assets/javascripts/angular/controllers/recommendation_group_controller.js
+++ app/assets/javascripts/angular/controllers/recommendation_group_controller.js
@@ -1,8 +1,24 @@
-app.controller('RecommendationGroupController', ['$scope', '$modalInstance', 'group',
-    function($scope, $modalInstance, group) {
-        $scope.group = group;
+app.controller('RecommendationGroupController', ['$scope', '$modalInstance', 'RecommendationGroup', 'group',
+    function ($scope, $modalInstance, RecommendationGroup, group) {
+        $scope.group = RecommendationGroup.get({groupId: group.id});
 
-        $scope.showRestaurant = function() {
+        $scope.hasNoShow = function () {
+            return group.recommendations && group.recommendations[0].hasOwnProperty("no_show_restaurant");
+        }
+
+        $scope.hasTried = function () {
+            return group.recommendations && group.recommendations[0].hasOwnProperty("tried_restaurant");
+        }
+
+        $scope.showSource = function () {
             $modalInstance.close();
         }
-}]);
+
+        $scope.showRestaurant = function () {
+            $modalInstance.close();
+        }
+
+        $scope.closeWindow = function () {
+            $modalInstance.close();
+        }
+    }]);
