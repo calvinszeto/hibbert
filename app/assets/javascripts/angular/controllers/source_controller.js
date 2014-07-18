91e46a437d5a3938a929aa2382c87e92f6323d67
Calvin Szeto <szeto.calvin@gmail.com>
2014-07-18 10:35:52 -0500

    Fix source card to match restaurant card styling.

--- app/assets/javascripts/angular/controllers/source_controller.js
+++ app/assets/javascripts/angular/controllers/source_controller.js
@@ -1,8 +1,18 @@
-app.controller('SourceController', ['$scope', 'Source', 'source',
-    function ($scope, Source, source) {
+app.controller('SourceController', ['$scope', 'Source', 'source', '$modalInstance',
+    function ($scope, Source, source, $modalInstance) {
         $scope.source = Source.get({sourceId: source.id});
 
         $scope.showGroup = function() {
             $modalInstance.close();
+        }
+
+        $scope.closeWindow = function() {
+            $modalInstance.close();
+        }
+
+        $scope.updateNoShow = function() {
+            var operation = source.no_show ? "add" : "remove";
+            $rootScope.User.updatePreferences({operation: operation, no_show_source: source.id});
+            SourcesQuery.refresh = true;
         }
     }]);
